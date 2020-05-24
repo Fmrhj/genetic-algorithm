87eb36166c1773d73512f0dbd17ed4af22721cfc
Fernando XPS <ferna.zepeda@gmail.com>
2020-05-24 20:52:05 +0200

    add README, jupyter notebook and final classes

--- README.md
+++ README.md
@@ -1,3 +1,84 @@
 # Genetic Algorithm Class 
-Python class for a genetic algorithm to solve an optimization problem with `n` control variables 
+Python implementation of a genetic algorithm to solve optimization problems with `n` control variables.
 
+## Description 
+A [genetic algorithm](https://en.wikipedia.org/wiki/Genetic_algorithm) (GA) is a search heuristic part of a broader family of algorithms called [evolutionary algorithms](https://en.wikipedia.org/wiki/Evolutionary_algorithm) (EAs). EAs are population-based metaheuristics that integrate mechanisms inspired by biological evolution such as reproduction, mutation, selection. The GA algorithm  is used particularly in optimization problems where calculating gradients of an objective function is problematic or not possible. 
+
+### Steps in GA
+
+1. **Initialization**: initialize a population of *individuals* or candidate solutions to the problem. This initialization can be done by means of random selection. 
+2. **Selection**: calculate the best candidates based on a fitness function we want to optimize. We select the best `j` *parents* to combine. This parameter is arbitrary. 
+3. **Crossover**: we combine the parents of last step to produce an *offspring*. These are `s` new individuals in our population.  
+4. **Mutation**: Add *randomness* to the generated offspring. 
+5. **Replacement**: Select the `l` fittest candidates of the population to evaluate on the next epoch.
+
+![](public/genetic-algo.png)
+
+## GA implementation 
+
+### Hyperparameters 
+
+- Individuals: 
+    - `lower_bound`
+    - `upper_bound`
+    - `number_of_genes`
+
+- Population:
+    - `n_parents`
+    - `offspring_size`
+    - `size`
+
+- Evolution: 
+    - `epochs`
+
+### Dependencies 
+
+- [Numpy](https://numpy.org/) 
+
+### Example 
+
+An example fittness function could be something like this:
+
+```python 
+def fitness(x, y):
+    return x*(x-1)*np.cos(2*x-1)*np.sin(2*x-1)*(y-2)
+```
+![](public/fitness_function.png)
+
+We can limit our search to the domain ![formula](https://render.githubusercontent.com/render/math?math=x\in[-2,2],y\in[-2,2]) with `ind_parameters`. Likewise, we control the population parameters with the `pop_parameters` dictionary.
+
+```python 
+from ga import Individual, Population, Evolution, fitness
+
+ind_parameters = {'lower_bound': -2,
+                  'upper_bound': 2,
+                  'number_of_genes': 2}
+
+pop_parameters = {'n_parents': 6,
+                  'offspring_size':(2,2),
+                  'size': 10}
+
+evo = Evolution(fitness, Population, Individual, pop_parameters, ind_parameters, fitness)
+
+# Repeat the evolution step over 200 epochs
+epochs = 200
+
+# Record best score history 
+history = []
+for _ in range(epochs):
+    evo.step()
+    history.append(evo._best_score)
+    
+print(evo._best_individual, evo._best_score)
+```
+
+```python
+# Output
+[array([-1.52637873, -2.        ])] 7.4697265870418414
+``` 
+![](public/fitness_history.png)
+
+## References
+
+- [An Extensible Evolutionary Algorithm Example in Python](https://towardsdatascience.com/an-extensible-evolutionary-algorithm-example-in-python-7372c56a557b)
+- [Genetic Algorithm Implementation in Python](https://towardsdatascience.com/genetic-algorithm-implementation-in-python-5ab67bb124a6)
---
+++ ga.py
@@ -0,0 +1,95 @@
+import numpy as np
+
+
+def fitness(x, y):
+    return x*(x-1)*np.cos(2*x-1)*np.sin(2*x-1)*(y-2)
+
+class Individual:
+    def __init__(self, params, value=None):
+        if value is None:
+            self.value = [np.random.uniform(params['lower_bound'], params['upper_bound'], 1)[
+                0] for i in range(params['number_of_genes'])]
+        else:
+            self.value = value
+
+    def get_values(self):
+        return self.value
+
+
+class Population:
+    def __init__(self, individual_class, pop_parameters, ind_parameters, fitness_function, ind_values=None):
+        self.individual_class = individual_class
+        if ind_values is None:
+            self.individuals = [individual_class(ind_parameters) for _ in range(pop_parameters['size'])]
+        else:
+            self.individuals = [individual_class(ind_parameters, value) for value in ind_values]
+        self.fitness_function = fitness_function
+        self.parents = None
+        self.size = pop_parameters['size']
+        self.n_parents = pop_parameters['n_parents']
+        self.offspring_size = pop_parameters['offspring_size']
+
+    def get_parents(self, n_parents):
+        parents = [self.individuals[i].get_values() for i in range(self.size)]
+        parents.sort(key=lambda x: self.fitness_function(
+            x[0], x[1]), reverse=True)
+        return np.array(parents[:n_parents])
+
+    def crossover(self, parents):
+        offspring = np.empty(self.offspring_size)
+        crossover_point = np.uint8(self.offspring_size[0]/2)
+        for k in range(self.offspring_size[1]):
+            parent1_idx = k % parents.shape[0]
+            # Index of the second parent to mate
+            parent2_idx = (k+1) % parents.shape[0]
+            # Half of the first parent
+            offspring[k, 0:crossover_point] = parents[parent1_idx,
+                                                      0:crossover_point]
+            # Half if the second parent
+            offspring[k, crossover_point:] = parents[parent2_idx,
+                                                     crossover_point:]
+        return offspring
+        
+    def mutate(self, offspring_crossover, ind_params):
+        for idx in range(offspring_crossover.shape[0]):
+            # select randomly the gene where randomness is going to be added
+            g = np.random.choice(range(offspring_crossover.shape[0]))
+
+            # The random value to be added to the gene.
+            offspring_crossover[idx][g] = offspring_crossover[idx][g] + \
+                np.random.normal(0.2, 0.25, 1)
+
+            # apply upper and lower bounds
+            if offspring_crossover[idx][g] > ind_params['upper_bound']:
+                offspring_crossover[idx][g] = ind_params['upper_bound']
+
+            if offspring_crossover[idx][g] < ind_params['lower_bound']:
+                offspring_crossover[idx][g] = ind_params['lower_bound']
+
+        return offspring_crossover
+
+class Evolution:
+    def __init__(self, fitness, population_class, individual_class, pop_parameters, ind_parameters, fitness_function):
+        self.fitness = fitness_function
+        self.population_class = population_class
+        self.individual_class = individual_class
+        self.population = population_class(
+            individual_class, pop_parameters, ind_parameters, fitness_function)
+        self.ind_parameters = ind_parameters
+        self.pop_parameters = pop_parameters
+        self._best_individual = None
+        self._best_score = None
+
+    def step(self):
+        parents = self.population.get_parents(self.pop_parameters['n_parents'])
+        offspring = self.population.crossover(parents)
+        mutation = self.population.mutate(offspring, self.ind_parameters)
+        population_values = [self.population.individuals[i].get_values()
+                             for i in range(self.population.size)]
+        population_values.extend(mutation)
+        population_values.sort(key=lambda x: self.fitness(x[0], x[1]), reverse=True)
+        population_values = population_values[:self.pop_parameters['size']]
+        self._best_individual = population_values[:1]
+        self._best_score = self.fitness(self._best_individual[0][0], self._best_individual[0][1])
+        # update population
+        self.population = self.population_class(self.individual_class, self.pop_parameters, self.ind_parameters, self.fitness, population_values)
