cf33fca256a6cdd12590688ac42676b530a8855b
Fernando Zepeda <fernando.zepeda@xing.com>
2021-12-02 21:32:22 +0100

    use interfaces for solution fetch

--- example.py
+++ example.py
@@ -20,21 +20,22 @@ def example():
     # Instantiate an evolution
     evo = Evolution(pop_parameters, ind_parameters, fitness)
     # Repeat evolution step 200 epochs
-    epochs = 10000
+    EPOCHS = 10000
     # Record fitness history 
     history = []
     x_history = []
     y_history = []
-    for _ in range(epochs):
-        print('Epoch {}/{}, Progress: {}%\r'.format(_+1, epochs, np.round(((_+1)/epochs)*100, 2)), end="")
+    for _ in range(EPOCHS):
+        print('Epoch {}/{}, Progress: {}%\r'.format(_+1, EPOCHS, np.round(((_+1)/EPOCHS)*100, 2)), end="")
         evo.step()
-        history.append(evo._best_score)
-        x_history.append(evo._best_individual[0][0])
-        y_history.append(evo._best_individual[0][1])
+        history.append(evo.solution.best_score)
+        x_history.append(evo.solution.best_individual[0])
+        y_history.append(evo.solution.best_individual[1])
     
-    print('\nResults:')
-    print('Best individual:', evo.solution.best_individual)
-    print('Fitness value of best individual:', evo.solution.best_score)
+    if evo.solution:
+        print('\nResults:')
+        print('Best individual:', evo.solution.best_individual)
+        print('Fitness value of best individual:', evo.solution.best_score)
 
 example()
     
